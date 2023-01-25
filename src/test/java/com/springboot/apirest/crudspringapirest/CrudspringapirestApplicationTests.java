f407f4ed332ac971e7c5043bf4dd66d8e3629d27
Alex Paranaguá <alexparanagua@gmail.com>
2023-01-25 19:01:46 -0300

    init

---
+++ src/test/java/com/springboot/apirest/crudspringapirest/CrudspringapirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.apirest.crudspringapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudspringapirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
